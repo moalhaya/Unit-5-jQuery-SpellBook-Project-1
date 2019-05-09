7c149f00eaf75edf95d230b4037652b2e7ecebb0
moalhaya <47435099+moalhaya@users.noreply.github.com>
2019-05-09 14:40:54 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+//Spell #1
+$("#copperfield").click(function(){
+    $("#hand-magic").toggle();
+ 
+});
+
+//Spell #2
+
+$("#copperfield").click(function(){
+    $("#hand-magic").slideToggle();
+ 
+});
+
+//Spell #3
+
+$("#copperfield").click(function(){
+    $("#Bio").fadeToggle();
+ 
+});
+
+
+
